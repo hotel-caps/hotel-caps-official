1a313af1949456dc9d5ccea2c332f96ec73027b8
krizzzcode <krizzzcode@gmail.com>
2026-09-15 03:21:52 +0530

    footer logo dimensions

--- components/Footer.vue
+++ components/Footer.vue
@@ -34,7 +34,7 @@ const footerNav = ref([
 
       <div class="flex justify-center items-center">
         <NuxtLink to="/" class="flex flex-col items-center">
-          <img class="h-20 w-auto" src="/images/caps-solid-logo.png" alt="Hotel CAPS Logo" />
+          <img class="h-[5rem] w-[5.618rem]" src="/images/caps-solid-logo.png" alt="Hotel CAPS Logo" />
           <div class="text-white flex flex-col items-center font-display">
             <p class="text-xl font-medium tracking-widest leading-loose">HOTEL</p>
             <p class="text-3xl font-bold tracking-widest -mt-1">CAPS</p>
